6bce880813554e966643ef50b26f3169515aae2d
KaOSrp19 <kevinramos28101999@hotmail.com>
2021-09-15 08:29:51 -0400

    Suma de los elementos de un Vector

--- src/Factorial.java
+++ src/Factorial.java
@@ -64,5 +64,21 @@ public class Factorial {
         return mcd;
     }
 
+    /*suma de los elementos de un vector
+    * vector = {2, 4, 6}
+    * suma = 2 + {4, 6}
+    * suma = 4 + {6}
+    * suma = 6(caso base)*/
+
+    public  static  int sumaVector(int[] vector, int indice){
+        int suma;
+        if (indice == vector.length - 1){
+            //caso base
+            suma = vector[indice];
+        }else{
+            suma = vector[indice] + sumaVector(vector,indice + 1);
+        }
+        return suma;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
         //System.out.println("METODO CONVENCIAONAL: " + Factorial.factorialSinRecursividad(5));
         //System.out.println("METODO RECURSIVO: " +Factorial.factorial(5));
 
-        System.out.println(Factorial.mcd(412,184));
+        //System.out.println(Factorial.mcd(412,184));
+
+        int[] vector={2,4,6};
+        System.out.println(Factorial.sumaVector(vector,0));
     }
 
 }
